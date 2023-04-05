191fee15ac1d6900017110fd901fbc07bd9b4eec
aritaoliveira <102245591+aritaoliveira@users.noreply.github.com>
2023-04-05 18:17:17 +0100

    mais cenas

--- src/functions.c
+++ src/functions.c
@@ -3,20 +3,52 @@
 
 #include "functions.h"
 
+void write_log(char* msg) {
+    char* text = NULL, current_time = NULL;
+    time_t t;
+    time(&t);
+    FILE *log;
+
+    current_time = (char *)malloc(strlen(ctime(&t)) * sizeof(char)+1);
+    strcpy(current_time, ctime(&t));
+    
+    text = (char *)malloc((strlen(current_time) + strlen(msg)) * sizeof(char)+1);
+    sprintf(text, "%s %s", current_time, msg);
+
+    log = fopen("log.txt", "a");
+
+    sem_wait(log_mutex);
+    fprintf(log, "%s", text);
+    fclose(log);
+    fflush(log);
+    fflush(stdout);
+    sem_post(log_mutex);
+    printf("%s", text);
+
+    free(current_time);
+    free(text);
+}
+
 void worker() {
 #if DEBUG
-    char* text;
-    sprintf(text, "Worker successfully created with the pid %d.", getpid());
+    char* text = NULL;
+    int pid = getpid();
+    text = (char *)malloc((strlen("Worker successfully created with the pid.") + sizeof(pid)) * sizeof(char)+1);
+    sprintf(text, "Worker successfully created with the pid %d.", pid);
     write_log(text);
+    free(text);
 #endif
     exit(0);
 }
 
 void alerts_watcher() {
 #if DEBUG
-    char* text;
-    sprintf(text, "Alerts Watcher successfully created with the pid %d.", getpid());
+    char* text = NULL;
+    int pid = getpid();
+    text = (char *)malloc((strlen("Alerts Watcher successfully created with the pid.") + sizeof(pid)) * sizeof(char)+1);
+    sprintf(text, "Alerts Watcher successfully created with the pid %d.", pid);
     write_log(text);
+    free(text);
 #endif
     exit(0);
 }
@@ -34,5 +66,15 @@ void system_manager() {
         exit(0);
     }
 
-    //criacao de threads
+    // criacao de threads
+
+
+    // inicializacao do semaforo log
+    void init_sem() {
+    sem_unlink("SEM_LOG");
+    sem_log = sem_open("SEM_LOG", O_CREAT | O_EXCL , 0700, 1);
+    if(sem_log == SEM_FAILED){
+        write_log("Erro %d ao inicializar o semáforo\n", errno);
+    }
+}
 }
--- src/functions.h
+++ src/functions.h
@@ -3,3 +3,13 @@
 #define DEBUG 1
 #define BUFFER_SIZE 512
 #define LOG_FILE "log.txt"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+sem_t *sem_log;
+
